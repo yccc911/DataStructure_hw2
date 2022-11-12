980f447789053e2d93be4e8f6b8aa53c2df68cb8
yccc911 <a85948533@gmail.com>
2022-11-12 16:02:50 +0800

    2022/11/12

---
+++ InsertionSort/InsertionSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <random>
+#include "SourceCode.h"
+
+using namespace std;
+
+int main(){
+
+    ofstream result("InsertionSort.result", ios::out);
+
+    for(int time=0; time<10; ++time){
+        for(int i=15; i<30; ++i){
+            int size = pow(2, i);
+            int *arr = new int[size];
+
+            random_device seed;
+            default_random_engine generator(seed());
+            uniform_int_distribution<int> uniform(1, 1000);
+            for(int j=0; j<size; ++j){
+                arr[j] = uniform(generator);
+            }
+
+            clock_t start = clock();
+            // sortint...
+            insertionSort(arr, size);
+            clock_t end = clock();
+            result << (double)(end - start)/CLOCKS_PER_SEC << endl;
+
+            delete []arr;
+        }
+        result << endl;
+    }
+
+    result.close();
+    return 0;
+}
